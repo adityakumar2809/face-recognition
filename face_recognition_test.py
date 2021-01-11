4f82bd08d02f4e6c050316e27d17cf5c03aca5fb
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-11 23:56:58 +0530

    Add feature to load npy files

--- face_recognition_test.py
+++ face_recognition_test.py
@@ -7,8 +7,15 @@ def getCascadeClassifier():
     return haar_cascade
 
 
+def loadFeaturesAndLabels():
+    features = np.load('trained_data/features.npy')
+    labels = np.load('trained_data/labels.npy')
+    return features, labels
+
+
 def main():
     haar_cascade = getCascadeClassifier()
+    features, labels - loadFeaturesAndLabels()
 
 
 if __name__ == "__main__":
