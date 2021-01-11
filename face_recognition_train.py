01307e291585d8ddb33746e9a22ed905f1cca89d
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-11 23:13:20 +0530

    Add code to get names of people

--- face_recognition_train.py
+++ face_recognition_train.py
@@ -3,8 +3,18 @@ import cv2
 import numpy as np
 
 
+def getFaceNames():
+    people_names = []
+    cwd = os.path.dirname(__file__)
+    pic_dir = os.path.join(cwd, 'images\\faces\\train')
+    for name in os.listdir(pic_dir):
+        people_names.append(name)
+    return people_names
+
+
 def main():
-    pass
+    people_names = getFaceNames()
+    print(people_names)
 
 
 if __name__ == "__main__":
