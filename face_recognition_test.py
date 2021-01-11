b18a7cce19ad13c3148ced8dfd0cb763582081ee
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-11 23:55:29 +0530

    Add code to import classifier

--- face_recognition_test.py
+++ face_recognition_test.py
@@ -2,8 +2,13 @@ import cv2
 import numpy as np
 
 
+def getCascadeClassifier():
+    haar_cascade = cv2.CascadeClassifier('haar_face.xml')
+    return haar_cascade
+
+
 def main():
-    pass
+    haar_cascade = getCascadeClassifier()
 
 
 if __name__ == "__main__":
