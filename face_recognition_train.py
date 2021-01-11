a43fe9a2093a0262d6920ea262af53b61da67a79
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-11 23:16:27 +0530

    Add function to extract directory name

--- face_recognition_train.py
+++ face_recognition_train.py
@@ -3,17 +3,22 @@ import cv2
 import numpy as np
 
 
-def getFaceNames():
-    people_names = []
+def getPicDir():
     cwd = os.path.dirname(__file__)
     pic_dir = os.path.join(cwd, 'images\\faces\\train')
+    return pic_dir
+
+
+def getFaceNames(pic_dir):
+    people_names = []
     for name in os.listdir(pic_dir):
         people_names.append(name)
     return people_names
 
 
 def main():
-    people_names = getFaceNames()
+    pic_dir = getPicDir()
+    people_names = getFaceNames(pic_dir)
     print(people_names)
 
 
