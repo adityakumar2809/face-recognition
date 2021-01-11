d40ac63a9abea290b0eb3215446b6c47018060bb
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-12 00:16:59 +0530

    Add face recognizing code

--- face_recognition_test.py
+++ face_recognition_test.py
@@ -41,17 +41,65 @@ def getGrayscaleImage(img):
     return gray_image
 
 
+def recognize_face(img, cascade_classifier, face_recognizer, people_names):
+    faces_rect = cascade_classifier.detectMultiScale(
+        image=img,
+        scaleFactor=1.1,
+        minNeighbors=4
+    )
+
+    for (x, y, w, h) in faces_rect:
+        faces_roi = img[y: y + h, x: x + w]
+
+        label, confidence = face_recognizer.predict(faces_roi)
+        print(
+            f'Label = {people_names[label]} with a confidence of {confidence}'
+        )
+
+        cv2.putText(
+            img=img,
+            text=str(people_names[label]),
+            org=(20, 20),
+            fontFace=cv2.FONT_HERSHEY_COMPLEX,
+            fontScale=1.0,
+            color=(0, 255, 0),
+            thickness=2
+        )
+
+        cv2.rectangle(
+            img=img,
+            pt1=(x, y),
+            pt2=(x + w, y + h),
+            color=(0, 255, 0),
+            thickness=2
+        )
+
+    return img
+
+
 def main():
     haar_cascade = getCascadeClassifier()
-    features, labels - loadFeaturesAndLabels()
+    # features, labels - loadFeaturesAndLabels()
 
     face_recognizer = createAndReadFaceRecognizer()
 
     pic_dir = getPicDir()
     people_names = getFaceNames(pic_dir)
 
-    img = cv2.imread(r'../images\faces\val\ben_afflek/1.jpg')
+    img = cv2.imread('images/faces/val/ben_afflek/1.jpg')
     gray_image = getGrayscaleImage(img)
+
+    cv2.imshow('Unidentified Person', gray_image)
+
+    detected_image = recognize_face(
+        gray_image,
+        haar_cascade,
+        face_recognizer,
+        people_names
+    )
+    cv2.imshow('Identified Person', detected_image)
+
+    cv2.waitKey(0)
 
 
 if __name__ == "__main__":
