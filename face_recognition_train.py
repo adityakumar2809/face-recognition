6f2f3fc07318dac62e7dbf9601477379e7467176
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-11 23:43:40 +0530

    Add code to create a training set

--- face_recognition_train.py
+++ face_recognition_train.py
@@ -33,15 +33,30 @@ def detectFace(cascade_classifier, img):
     faces_rect = cascade_classifier.detectMultiScale(
         image=img,
         scaleFactor=1.1,
-        minNeighbors=10
+        minNeighbors=4
     )
     return faces_rect
 
 
-def getCroppedFaces(img, faces_rect):
-    for (x, y, w, h) in faces_rect:
-        faces_roi = img[y: y + h, x: x + w]
-    return faces_roi
+def createTrainSet(people_names, pic_dir, cascade_classifier):
+    features = []
+    labels = []
+    for person in people_names:
+        path = os.path.join(pic_dir, person)
+        label = people_names.index(person)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path, img)
+
+            bgr_image = cv2.imread(img_path)
+            gray_image = getGrayscaleImage(bgr_image)
+
+            faces_rect = detectFace(cascade_classifier, gray_image)
+            for (x, y, w, h) in faces_rect:
+                faces_roi = gray_image[y: y + h, x: x + w]
+                features.append(faces_roi)
+                labels.append(label)
+    return features, labels
 
 
 def main():
@@ -51,6 +66,10 @@ def main():
     print(people_names)
 
     haar_cascade = getCascadeClassifier()
+
+    features, labels = createTrainSet(people_names, pic_dir, haar_cascade)
+    print(f'Length of features = {len(features)}')
+    print(f'Length of labels = {len(labels)}')
 
 
 if __name__ == "__main__":
